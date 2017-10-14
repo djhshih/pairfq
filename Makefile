fac33a48e8157860a6185a724fcec8a97392db18
David J. H. Shih <djh.shih@gmail.com>
2017-10-14 14:38:17 -0400

    Initial commit.

---
+++ Makefile
@@ -0,0 +1,22 @@
+CXX=g++
+CPPFLAGS=-O3
+
+all: pairfq
+	
+
+pairfq: pairfq.cpp
+	$(CXX) $(CPPFLAGS) $? -o $@
+
+coverage: pairfq.cpp
+	$(CXX) -coverage -O0 $? -o $@
+	./coverage
+	gcov $?
+
+check: pairfq
+	mkdir -p out
+	./pairfq data/r1.fq data/r2.fq out/out.fq out/unpaired.fq
+	diff data/out.fq out/out.fq
+	diff data/unpaired.fq out/unpaired.fq
+
+test: check
+	
---
+++ pairfq.cpp
@@ -0,0 +1,222 @@
+#include <fstream>
+#include <iostream>
+#include <deque>
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
+
+struct fastq_entry {
+    /// read name
+    string qname;
+    /// read sequence
+    string seq;
+    /// read quality scores
+    string qual;
+};
+
+/**
+ * Natural string comparison, account for numbers within a string.
+ * This is different from lexigraphical comparison (e.g. std::string::compare).
+ * Taken from https://github.com/samtools/samtools/blob/develop/bam_sort.c#L13
+ */
+int strnum_cmp(const char *_a, const char *_b) {
+    const unsigned char *a = (const unsigned char*)_a, *b = (const unsigned char*)_b;
+    const unsigned char *pa = a, *pb = b;
+    while (*pa && *pb) {
+        if (isdigit(*pa) && isdigit(*pb)) {
+            while (*pa == '0') ++pa;
+            while (*pb == '0') ++pb;
+            while (isdigit(*pa) && isdigit(*pb) && *pa == *pb) ++pa, ++pb;
+            if (isdigit(*pa) && isdigit(*pb)) {
+                int i = 0;
+                while (isdigit(pa[i]) && isdigit(pb[i])) ++i;
+                return isdigit(pa[i])? 1 : isdigit(pb[i])? -1 : (int)*pa - (int)*pb;
+            } else if (isdigit(*pa)) return 1;
+            else if (isdigit(*pb)) return -1;
+            else if (pa - a != pb - b) return pa - a < pb - b? 1 : -1;
+        } else {
+            if (*pa != *pb) return (int)*pa - (int)*pb;
+            ++pa; ++pb;
+        }
+    }
+    return *pa? 1 : *pb? -1 : 0;
+}
+
+/**
+ * Compare two read names by natural order.
+ */
+int compare_qnames(const string& a, const string& b) {
+    size_t a_end = a.length();
+    size_t b_end = b.length();
+    if (a.substr(a_end - 2) == "/1") {
+        a_end -= 2;
+    }
+    if (b.substr(b_end - 2) == "/2") {
+        b_end -= 2;
+    }
+    
+    return strnum_cmp(a.substr(0, a_end).c_str(), b.substr(0, b_end).c_str());
+}
+
+/** 
+ * Read one fastq entry from file.
+ *
+ * Assume that each sequence and quality score entry `r1.fq` and `r2.fq` is single-line.
+ */
+bool read_fastq_entry(istream& f, fastq_entry& x) {
+    getline(f, x.qname);
+    if (x.qname.empty()) return false;
+    
+    getline(f, x.seq);
+
+    string marker;
+    getline(f, marker);
+    if (marker != "+") {
+        throw runtime_error("fastq entry is malformed");
+    }
+
+    getline(f, x.qual);
+    
+    return true;
+}
+
+/** 
+ * Write one fastq entry to file.
+ */
+void write_fastq_entry(ostream& f, const fastq_entry& x) {
+    f << x.qname << endl;
+    f << x.seq << endl;
+    f << '+' << endl;
+    f << x.qual << endl;
+}
+
+/** 
+ * Flush all fastq entries from deque and writea them to file.
+ */
+void flush_fastq_entries(ostream& f, deque<fastq_entry>& q) {
+    while(!q.empty()) {
+        write_fastq_entry(f, q.back());
+        q.pop_back();
+    }
+}
+
+/**
+ * Read the next fastq entry from file and push onto front of the deque.
+ */
+inline bool read_next_fastq(istream& f, deque<fastq_entry>& q) {
+    q.push_front(fastq_entry());
+    if (read_fastq_entry(f, q.front())) {
+        return true;
+    }
+    // read failed: remove empty entry
+    q.pop_front();
+    return false;
+}
+
+/**
+ * Pair first-read and second-second fastq files together, separating unpaired reads.
+ *
+ * usage: pairfq <r1.fq> <r2.fq> <out.fq> [unpaired.fq]
+ * 
+ * Assume that each sequence and quality score entry `r1.fq` and `r2.fq` is single-line.
+ * Assume that `r1.fq` and `r2.fq` are both sorted naturally.
+ * (e.g. the source BAM file was sorted by `sambamba sort -N` or `samtools sort -n`).
+ *
+ * Unpaired reads will be written to `unpaired.fq`, if available.
+ */
+int main(int argc, char* argv[]) {
+
+    char* r1_fname;
+    char* r2_fname;
+    char* out_fname;
+    char* unpaired_fname = NULL;
+
+    int nargs = argc - 1;
+    if (nargs < 3) {
+        cerr << "usage: pairfq <r1.fq> <r2.fq> <out.fq> [unpaired.fq]" << endl;
+        return 1;
+    } else {
+        r1_fname = argv[1];
+        r2_fname = argv[2];
+        out_fname = argv[3];
+        if (nargs >= 4) {
+            unpaired_fname = argv[4];
+        }
+    }
+    
+    // open input and output files
+    
+    ifstream r1f(r1_fname);
+    ifstream r2f(r2_fname);
+    ofstream outf(out_fname);
+    ofstream unpairedf;
+    bool write_unpaired = false;
+    if (unpaired_fname != NULL) {
+        unpairedf.open(unpaired_fname);
+        write_unpaired = true;
+    }
+    
+    // process reads
+    
+    deque<fastq_entry> r1q, r2q;
+
+    while (true) {
+        if (r1q.empty()) {
+            // get the next fastq entry from list 1
+            if (!read_next_fastq(r1f, r1q)) break;
+        }
+
+        if (r2q.empty()) {
+            // get the next fastq entry from list 2
+          if (!read_next_fastq(r2f, r2q)) break;
+        }
+        
+        int cmp = compare_qnames(r1q.front().qname, r2q.front().qname);
+
+        if (cmp < 0) {
+            // current element of list 1 is smaller:
+            // get the next element of list 2 for comparison
+            if (!read_next_fastq(r1f, r1q)) break; 
+        } else if (cmp > 0) {
+            // current element of list 2 is smaller:
+            // get the next element of list 1 for comparison
+            if (!read_next_fastq(r2f, r2q)) break; 
+        } else {
+            // elements of list 1 and list 2 match: write matched pair to file
+            write_fastq_entry(outf, r1q.front());
+            r1q.pop_front();
+            write_fastq_entry(outf, r2q.front());
+            r2q.pop_front();
+            // deques now possibly contain unpaired reads: write them out
+            if (write_unpaired) {
+                flush_fastq_entries(unpairedf, r1q);
+                flush_fastq_entries(unpairedf, r2q);
+            } else {
+                r1q.clear();
+                r2q.clear();
+            }
+        }
+    }
+    
+    // flush the remaining reads
+    if (write_unpaired) {
+        flush_fastq_entries(unpairedf, r1q);
+        flush_fastq_entries(unpairedf, r2q);
+    } else {
+        r1q.clear();
+        r2q.clear();
+    }
+
+    // clean up
+
+    r1f.close();
+    r2f.close();
+    outf.close();
+    if (write_unpaired) {
+        unpairedf.close();
+    }
+
+    return 0;
+}
